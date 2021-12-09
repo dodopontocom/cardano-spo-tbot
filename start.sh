13da110878be65f567b4af2409afa0d28a923060
Rodolfo Neto <rodolfotiago@gmail.com>
2021-12-09 15:16:02 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export BASEDIR="$(cd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1 && pwd)"
+BASEDIR="$(cd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1 && pwd)"
 
 TELEGRAM_TOKEN="$1"
 TELEGRAM_ID=($2)
