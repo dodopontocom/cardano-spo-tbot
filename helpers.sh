804116cf975f38c785cc26d71fa81ab00f86d53b
dodopontocom <rodolfotiago@gmail.com>
2021-12-10 09:23:06 -0300

    helpers

---
+++ helpers.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+BASEDIR="$(cd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1 && pwd)"
+
+TELEGRAM_TOKEN="$1"
+TELEGRAM_ID=($2)
+SHELL_BOT_API_URL="https://github.com/shellscriptx/shellbot.git"
+SHELL_BOT_PATH=${BASEDIR}/shellbot
+
+helper.get_api() {
+  echo "[INFO] Providing the API for the bot's project folder"
+  ls ${SHELL_BOT_PATH} > /dev/null 2>&1 || git clone ${SHELL_BOT_API_URL} ${SHELL_BOT_PATH} > /dev/null 2>&1
+}
