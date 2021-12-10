36ce380d2d87f44868ddb883f6a7ad3c89ce8ba6
dodopontocom <rodolfotiago@gmail.com>
2021-12-10 09:40:13 -0300

    helpers

--- helpers.sh
+++ helpers.sh
@@ -11,3 +11,13 @@ helper.get_api() {
   echo "[INFO] Providing the API for the bot's project folder"
   ls ${SHELL_BOT_PATH} > /dev/null 2>&1 || git clone ${SHELL_BOT_API_URL} ${SHELL_BOT_PATH} > /dev/null 2>&1
 }
+
+bot.cardano_check_latest_version() {
+    #it will return 0 if latest version (from releases raw page)
+    # is greater than the current version installed in the server
+    versions=( \
+        "$(curl -s https://api.github.com/repos/input-output-hk/cardano-node/releases/latest | jq -r .tag_name)" \
+        "$(cardano-node --version | awk '{ print $2 }' | head -1)" \
+    )
+    test "$(echo "${versions[@]}" | tr " " "\n" | sort -V | head -n 1)" != "${versions[0]}"
+}
--- tbot-start.sh
+++ tbot-start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-BASEDIR="$(cd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1 && pwd)"
+export BASEDIR="$(cd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1 && pwd)"
+
+source ${BASEDIR}/helpers.sh
 
 TELEGRAM_TOKEN="$1"
 TELEGRAM_ID=($2)
